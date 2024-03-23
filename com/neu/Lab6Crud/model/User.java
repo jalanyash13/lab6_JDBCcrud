6c99bfeaa53b1f33f2891c19c01967851748073c
jalanyash <57186641+jalanyash@users.noreply.github.com>
2024-03-23 11:23:42 +0530

    Initial Commit

---
+++ com/neu/Lab6Crud/model/User.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.neu.Lab6Crud.model;
+
+/**
+ *
+ * @author jalan
+ */
+public class User {
+    
+    String name;
+    int id;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}
